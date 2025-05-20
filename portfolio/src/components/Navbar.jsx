19f44d35dd9c97a4527ea3ad25425cb79c439791
shrutika-gawande <shrutikagawande80@gmail.com>
2025-05-20 17:13:25 +0530

    updated navbar links

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -12,19 +12,19 @@ const Navbar = () => {
         SG
       </div>
       <div className="hidden sm:flex space-x-8">
-        <a href="home" className="relative group">
+        <a href="#home" className="relative group">
           <span className="text-white">Home</span>
           <span className="absolute left-0 -bottom-1 h-0.5 w-0 bg-purple-400 transition-all duration-300 group-hover:w-full"></span>
         </a>
-        <a href="about" className="relative group">
+        <a href="#about" className="relative group">
           <span className="text-white">About</span>
           <span className="absolute left-0 -bottom-1 h-0.5 w-0 bg-purple-400 transition-all duration-300 group-hover:w-full"></span>
         </a>
-        <a href="projects" className="relative group">
+        <a href="#projects" className="relative group">
           <span className="text-white ">Projects</span>
           <span className="absolute left-0 -bottom-1 h-0.5 w-0 bg-purple-400 transition-all duration-300 group-hover:w-full"></span>
         </a>
-        <a href="contact" className="relative group">
+        <a href="#contact" className="relative group">
           <span className="text-white ">Contact Me</span>
           <span className="absolute left-0 -bottom-1 h-0.5 w-0 bg-purple-400 transition-all duration-300 group-hover:w-full"></span>
         </a>
@@ -39,10 +39,10 @@ const Navbar = () => {
       {isOpen && (
         <div className="absolute top-16 right-0 w-2/6 pl-7 max-w-xs mr-1 rounded-md backdrop-blur-md bg-white/20 shadow-md flex flex-col items-center pr-6 space-y-4 py-6 z-50 sm:hidden">
           {[
-            { label: "Home", href: "home" },
-            { label: "About", href: "about" },
-            { label: "Projects", href: "projects" },
-            { label: "Contact", href: "contact" },
+            { label: "Home", href: "#home" },
+            { label: "About", href: "#about" },
+            { label: "Projects", href: "#projects" },
+            { label: "Contact", href: "#contact" },
           ].map((item) => (
             <a
               key={item.href}
